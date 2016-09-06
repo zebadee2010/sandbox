1cab596d2ed362439a7d4eef7bc7549d4c889fb7
BuildTools <unconfigured@null.spigotmc.org>
2016-09-05 22:47:30 -0400

    add form info

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <title>Sandbox</title>
   </head>
   <body>
-    
+    <header>
+      <h1>Placeholder for header styling</h1>
+    </header>
+    <section>
+      <form class="form_style" action="index.html" method="post">
+        <input type="text" name="FirstName" value="firstname">
+        <input type="text" name="LastName" value="lastname">
+        <input type="button" name="Submit Button" value="Submission">
+      </form>
+    </section>
   </body>
 </html>
